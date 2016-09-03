c9b7803fca6d1817a413651dd8d20a38bb824ae6
Hari Dahal <hxd1478@rit.edu>
2016-09-03 10:54:39 -0400

    Clear setup

--- cQuiz/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class CquizConfig(AppConfig):
-    name = 'cQuiz'
--- cQuiz/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    context = {'mo_it': 'cQuiz', 'my_pages': ['Do 1', 'Do 2', 'Do 3', 'Do 4']}
-    return render(request, 'cQuiz/index.html', context=context)
