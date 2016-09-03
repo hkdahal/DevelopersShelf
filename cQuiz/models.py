1a0d0ac1be8499a508622d9e1d5d07cfe8f50efc
Hari Dahal <hxd1478@rit.edu>
2016-09-03 12:50:38 -0400

    to string for models updated

--- cQuiz/models.py
+++ cQuiz/models.py
@@ -4,12 +4,17 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=1000)
     pub_date = models.DateTimeField('date published')
+    q_id = models.IntegerField()
+
+    def __str__(self):
+        return "{0}. {1}".format(self.q_id, self.question_text)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=500)
     correct = models.BooleanField(default=False)
+    c_id = models.IntegerField()
 
-
-
+    def __str__(self):
+        return "Q#{0} {1}. {2}".format(self.question.q_id, self.c_id, self.choice_text)
