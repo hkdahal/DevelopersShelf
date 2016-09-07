7f4ea9f2bde7adb62cb6621857d3e127b4b0186f
Hari Dahal <hxd1478@rit.edu>
2016-09-07 15:21:42 -0400

    Integreted Django Form and few Views tweak per Django Tutorial

--- cQuiz/views.py
+++ cQuiz/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
 from .models import Question, Choice
 
 
+class IndexView(generic.ListView):
+    template_name = 'cQuiz/cQuizIndex.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'cQuiz/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'cQuiz/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'cQuiz/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        if selected_choice.correct:
+            return HttpResponseRedirect(reverse('cQuiz:results', args=(question.id,)))
+
+
+        # selected_choice.votes += 1
+        # selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+
+
+
+"""
 def index(request):
     context = {'mo_it': 'cQuiz', 'my_pages': ['Do 1', 'Do 2', 'Do 3', 'Do 4']}
     return render(request, 'cQuiz/base.html', context=context)
@@ -11,3 +59,5 @@ def qa(request):
     questions = Question.objects.all()
     context = {'mo_it': 'cQuiz', 'my_pages': ['Do 1', 'Do 2', 'Do 3', 'Do 4'], 'questions': questions}
     return render(request, 'cQuiz/tukra.html', context=context)
+"""
+
