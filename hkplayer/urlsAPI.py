dbb4bf705e1a3d40c497a0a1a78068ca75ba546e
Hari Dahal <hxd1478@rit.edu>
2016-09-10 12:14:09 -0400

    API Function: Songs CRUD

--- hkplayer/urlsAPI.py
+++ hkplayer/urlsAPI.py
@@ -4,7 +4,18 @@ from . import viewsAPI
 
 urlpatterns = [
     url(r'^$', viewsAPI.ListCreateMovie.as_view(), name='movie_list'),
-    url(r'(?P<pk>\d+)/$',
+
+    url(r'^(?P<pk>\d+)/$',
         viewsAPI.RetrieveUpdateDestroyMovie.as_view(),
         name="movie_detail"),
+
+    url(r'^(?P<movie_pk>\d+)/songs/$',
+        viewsAPI.ListCreateSong.as_view(),
+        name='song_list'),
+
+    url(r'^(?P<movie_pk>\d+)/songs/(?P<pk>\d+)/$',
+        viewsAPI.RetrieveUpdateDestroySong.as_view(),
+        name='song_detail'
+        )
 ]
+
--- hkplayer/viewsAPI.py
+++ hkplayer/viewsAPI.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import generics
 
 from . import models
@@ -12,3 +14,29 @@ class ListCreateMovie(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyMovie(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
+
+
+class ListCreateSong(generics.ListCreateAPIView):
+    queryset = models.Song.objects.all()
+    serializer_class = serializers.SongSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(movie_id=self.kwargs.get('movie_pk'))
+
+    def perform_create(self, serializer):
+        movie = get_object_or_404(
+            models.Movie, pk=self.kwargs.get('movie_pk'))
+        serializer.save(movie=movie)
+
+
+class RetrieveUpdateDestroySong(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Song.objects.all()
+    serializer_class = serializers.SongSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.queryset,
+            movie_id=self.kwargs.get('movie_pk'),
+            pk=self.kwargs.get('pk')
+        )
+
